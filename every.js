5c309ff2229fb2e2a7b9e85e0cd3028741955326
andyzali <ninthsense@outlook.com>
2015-12-08 15:06:08 -0800

    Update every.js

--- every.js
+++ every.js
@@ -1,3 +1,45 @@
+var each = function (collection, callback) {
+
+	var i, 
+  		len, 
+  		prop;
+  
+  if (Array.isArray(collection)) {
+  	for (i = 0, len = collection.length; i < len; i += 1) {
+    	callback(collection[i]);
+    }
+  } else {
+  	for (prop in collection) {
+    	callback(collection[prop]);
+    }
+  }
+};
+
+var reduce = function (collection, accumulator, startValue) {
+  each(collection, function (item) {
+    startValue = accumulator(item, startValue);
+  });
+  return startValue;
+};
+
+var every = function (collection, predicate) {
+	return reduce(collection, function (curr) {
+  	if (!predicate(curr)) {
+    	return false;
+    } else {
+    	return true;
+    }
+  }); 
+};
+
+var arr = [8, 8, 9];
+
+var isEight = function (num) {
+  return num === 8;
+};
+
+every(arr, isEight);
+
 // functionally deficient every() construct --
 // true every() takes in a callbackFn and an optional thisArg parameter
 // utilizing the callbackFn to check on every element in the array
